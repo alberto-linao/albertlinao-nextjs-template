5851ba3c9ed23a6b64c50c29e367b49e2d0aef8e
Albert Linao <5449869+alberto-linao@users.noreply.github.com>
2022-10-31 01:37:13 +0800

    Add supabase util

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,13 @@ import type { AppProps } from "next/app";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
 import { useState } from "react";
-import Account from "../components/Account";
+import createClient from "../utils/supabase";
 
 export default function App({
   Component,
   pageProps,
 }: AppProps<{ initialSession: Session }>) {
-  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+  const supabaseClient = createClient;
 
   return (
     <SessionContextProvider
