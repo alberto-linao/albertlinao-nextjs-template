d426ac5ed8fe62ad9d38c169d32cfd42334fc724
Albert Linao <5449869+alberto-linao@users.noreply.github.com>
2022-11-08 01:43:30 +0800

    Add sample for nested describe

--- __tests__/Sample.test.tsx
+++ __tests__/Sample.test.tsx
@@ -1,10 +1,15 @@
 import { describe } from "@jest/globals";
-import add from "../components/Sample";
+import { add, subtract } from "../components/Sample";
 
 describe("Sample Componenet functions are correct", () => {
   describe("add function", () => {
     test("Add 1 + 2 equals 3", () => {
       expect(add(1, 2)).toBe(3);
+    });
+  });
+  describe("subtract function", () => {
+    test("Add 2 - 1 equals 1", () => {
+      expect(subtract(2, 1)).toBe(1);
     });
   });
 });
